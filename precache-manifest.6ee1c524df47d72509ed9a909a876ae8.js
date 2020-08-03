5e40ad81c3e13563ce890a2c4ab13315fd915327
tungnguyen1706 <nguyenthanhtung9990@gmail.com>
2020-08-03 18:59:55 +0700

    Updates

---
+++ precache-manifest.6ee1c524df47d72509ed9a909a876ae8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f5d2bd9a2c4724c02c9eb27b31f517",
+    "url": "/todoappredux.github.io/index.html"
+  },
+  {
+    "revision": "8c4c1a52acca6143e609",
+    "url": "/todoappredux.github.io/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "248795f4b64e3c4026e5",
+    "url": "/todoappredux.github.io/static/css/main.edebd46d.chunk.css"
+  },
+  {
+    "revision": "8c4c1a52acca6143e609",
+    "url": "/todoappredux.github.io/static/js/2.57de34b9.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/todoappredux.github.io/static/js/2.57de34b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "248795f4b64e3c4026e5",
+    "url": "/todoappredux.github.io/static/js/main.b0bcb374.chunk.js"
+  },
+  {
+    "revision": "632f8a4e486ac73f19ea",
+    "url": "/todoappredux.github.io/static/js/runtime-main.a406f885.js"
+  }
+]);
